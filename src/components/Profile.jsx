bed5d5c749bbcf5dd1286c4dd0ca1ec414108de9
andreiribac <ribacandrei@gmail.com>
2021-06-21 15:31:30 +0300

    add new components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+function Profile() {
+	return (
+		<section className="content">
+			<img src="https://st.depositphotos.com/1003348/2724/i/950/depositphotos_27241193-stock-photo-sunset-panorama.jpg" alt="" />
+			<div>
+				ava + description
+			</div>
+			<div>
+				My posts
+				<div>
+					New post
+				</div>
+				<div>
+					<div>post 1</div>
+					<div>post 2</div>
+					<div>post 3</div>
+				</div>
+			</div>
+		</section>
+
+	);
+}
+
+export default Profile;
