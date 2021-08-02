cec7c087bb569f7dd640ee4871ef8b36a981a0c5
andreiribac <ribacandrei@gmail.com>
2021-08-02 22:06:27 +0300

    72 lesson

--- src/components/Profile/ProfileInfo/ProfileStatus.js
+++ src/components/Profile/ProfileInfo/ProfileStatus.js
@@ -6,13 +6,13 @@ class ProfileStatus extends React.Component {
 	state = {
 		editMode: false,
 	}
-	activateEditMode() {
+	activateEditMode = () => {
 		this.setState({
 			editMode: true,
 		});
 		// this.forceUpdate();
 	}
-	deactivateEditMode() {
+	deactivateEditMode = () => {
 		this.setState({
 			editMode: false,
 		});
@@ -22,12 +22,12 @@ class ProfileStatus extends React.Component {
 			<>
 				{!this.state.editMode &&
 					<div>
-					<span onDoubleClick={this.activateEditMode.bind(this)}> {this.props.status}</span>
+					<span onDoubleClick={this.activateEditMode}> {this.props.status}</span>
 					</div>
 				}
 				{this.state.editMode &&
 					<div>
-						<input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} type="text" value={this.props.status} />
+						<input autoFocus={true} onBlur={this.deactivateEditMode} type="text" value={this.props.status} />
 					</div>
 				}
 			</>
